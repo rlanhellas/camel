13b0b52d1f3f3c4897b99ed32b5f6d8017c4db5e
ronaldo.lanhellas <desenvolvimento@emagesolucoes.com.br>
2019-05-12 17:08:14 -0300

    First commit

---
+++ src/main/java/br/com/lanhellas/camelapp/CamelAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.lanhellas.camelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/br/com/lanhellas/camelapp/config/CamelRoute.java
@@ -0,0 +1,13 @@
+package br.com.lanhellas.camelapp.config;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CamelRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+       // from("activemq:queue:MyQueue")
+     //           .to("file:/tmp/restaurants.out");
+    }
+}
